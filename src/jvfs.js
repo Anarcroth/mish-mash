c0bcc081ea6588e58205502fd3183d0d216e5622
Martin Nestorov <mdinestorov@gmail.com>
2018-09-23 23:39:19 +0300

    Added init command for vfs

--- src/jvfs.js
+++ src/jvfs.js
@@ -1,11 +1,22 @@
 const fs = require("fs");
+const path = require("path");
 
 let jayVFS = {
+    prompt: "",
+    promptColor: "",
+    bgColor: "",
+    termBgColor: "",
+    termlWidth: "",
+    termHeight: "",
+    outputColor: "",
+    outputBoldColor: "",
     dirs: [],
     files: [],
 
     init() {
-
+        let configFile = fs.readFile('../.conf', 'utf8', function(err, contents) {
+            console.log(contents);
+        });
     },
 
     openFile() {
